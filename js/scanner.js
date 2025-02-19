1a534ffd8cb2241165946766cffcfaa62c1312f2
Febri Prasetiyo <139911621+febrilym@users.noreply.github.com>
2025-02-20 06:09:52 +0700

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,106 +1,18 @@
-const scannerDiv = document.querySelector(".scanner");
+const generatorTab = document.querySelector(".nav-generator");
+const scannerTab = document.querySelector(".nav-scanner");
 
-const camera = scannerDiv.querySelector("h1 .fa-camera");
-const stopCam = scannerDiv.querySelector("h1 .fa-circle-stop");
+generatorTab.addEventListener("click", () => {
+    generatorTab.classList.add("active");
+    scannerTab.classList.remove("active");
 
-const form = scannerDiv.querySelector(".scanner-form");
-const fileInput = form.querySelector("input");
-const p = form.querySelector("p");
-const img = form.querySelector("img");
-const video = form.querySelector("video");
-const content = form.querySelector(".content");
-
-const textarea = scannerDiv.querySelector(".scanner-details textarea");
-const copyBtn = scannerDiv.querySelector(".scanner-details .copy");
-const closeBtn = scannerDiv.querySelector(".scanner-details .close");
-
-form.addEventListener("click", () => fileInput.click());
-
-fileInput.addEventListener("change", e => {
-    let file = e.target.files[0];
-    if (!file) return;
-    fetchRequest(file);
+    document.querySelector(".scanner").style.display = "none";
+    document.querySelector(".generator").style.display = "block";
 })
 
-function fetchRequest(file){
-    let formData = new FormData();
-    formData.append("file", file);
-
-    p.innerText = "Scanning QR Code...";
-
-    fetch(`http://api.qrserver.com/v1/read-qr-code/`, {
-        method: "POST", body: formData
-    }).then(res => res.json()).then(result => {
-        let text = result[0].symbol[0].data;
-
-        if (!text)
-            return p.innerText = "Tidak bisa scan QR Code :("
-        
-        scannerDiv.classList.add("active");
-        form.classList.add("active-img");
-
-        img.src = URL.createObjectURL(file);
-        textarea.innerText = text;
-    })
-}
-
-let scanner;
-
-camera.addEventListener("click", () => {
-    camera.style.display = "none";
-    form.classList.add("pointerEvents");
-    p.innerText = "Scanning QR Code...";
-
-    scanner = new Instascan.Scanner({video: video});
-    Instascan.Camera.getCameras()
-        .then(cameras => {
-            if (cameras.length > 0){
-                scanner.start(cameras[0]).then(() => {
-                    form.classList.add('active-video');
-                    stopCam.style.display = "inline-block";
-                })
-            }else{
-                console.log("Camera tidak ditemukan :(");
-            }
-        })
-        .catch(err => console.error(err))
-
-    scanner.addListener("scan", c => {
-        scannerDiv.classList.add("active");
-        textarea.innerText = c;
-    })
+scannerTab.addEventListener("click", () => {
+    scannerTab.classList.add("active");
+    generatorTab.classList.remove("active");
+    
+    document.querySelector(".scanner").style.display = "block";
+    document.querySelector(".generator").style.display = "none";
 })
-
-copyBtn.addEventListener("click", () => {
-    let text = textarea.textContent;
-    navigator.clipboard.writeText(text).then(() => {
-        showNotification("Text telah disalin!");
-    }).catch(err => {
-        console.error("Gagal menyalin :(", err);
-    });
-});
-
-function showNotification(message) {
-    const notification = document.querySelector(".notification");
-    notification.innerText = message;
-    notification.classList.add("show");
-
-    setTimeout(() => {
-        notification.classList.remove("show");
-    }, 2000);
-}
-
-closeBtn.addEventListener("click", () => stopScan());
-stopCam.addEventListener("click", () => stopScan());
-
-function stopScan(){
-    p.innerText = "Upload QR Code untuk Scan!";
-
-    camera.style.display = "inline-block";
-    stopCam.style.display = "none";
-
-    form.classList.remove("active-video", "active-img", "pointerEvents");
-    scannerDiv.classList.remove("active");
-
-    if (scanner) scanner.stop();
-}
